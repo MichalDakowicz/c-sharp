aa6a0143dc876728872669d8a583b0056946c7a4
Michu <michal.dakowicz@infotech.edu.pl>
2025-02-13 14:42:05 +0100

    c#

---
+++ zadania/nowe.cs
@@ -0,0 +1,41 @@
+// int a = 5;
+// int b = 10;
+
+// if (a < b)
+// {
+//     Console.WriteLine("a jest mniejsze niż b");
+// }
+// else
+// {
+//     Console.WriteLine("a jest większe niż b");
+// }
+
+// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// int x = 0;
+
+// while (x < 10)
+// {
+//     Console.WriteLine(x);
+//     x++;
+// }
+
+// int [] liczby = {1, 2, 3, 4, 5};
+
+// for (int i = 0; i < liczby.Length; i++)
+// {
+//     Console.WriteLine(liczby[i]);
+// }
+
+// int liczba = int.Parse(Console.ReadLine());
+
+// if (liczba > 0) {
+//     Console.WriteLine("Liczba jest dodatnia");
+// } else if (liczba < 0) {
+//     Console.WriteLine("Liczba jest ujemna");
+// } else {
+//     Console.WriteLine("Liczba jest równa zero");
+// }
